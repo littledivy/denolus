5d42e0fead8c228ec95b7aab241055d59d574ebc
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-17 14:42:04 +0530

    feat(compiler): setup compiler

---
+++ src/compiler/compiler.ts
@@ -0,0 +1,17 @@
+// TODO: better type checks
+
+class Css {
+  static of (json: any) {
+    const selectors = Object.keys(json)
+    return selectors.map((selector: any) => {
+      const definition = json[selector]
+      const rules = Object.keys(definition)
+      const result = rules.map((rule) => {
+        return `${rule}:${definition[rule]}`
+      }).join(';')
+      return `${selector}{${result}}`
+    }).join('\n')
+  }
+}
+
+export default Css;
