42dae8bb2aa6b877d7e8fb4805042ba736d01058
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-17 14:39:30 +0530

    feat(parser): setup a parse module

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,17 +1,5 @@
 import { parse } from '../../deps.ts';
 
-const result = parse(`
-test: toto
-foo:
-  bar: True
-  baz: 1
-  qux: ~
-`);
-console.log(result);
-
-const expected = '{ test: "toto", foo: { bar: true, baz: 1, qux: null } }';
-if (Deno.inspect(result) === expected) {
-    console.log('Output is as expected.');
-} else {
-    console.error('Error during parse. Output is not as expect.', expected);
+export function parseDenolus(code: string) {
+  return JSON.parse(parse(code));
 }
