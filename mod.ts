e28e3294cbd9e5b225e39bffcb94dbe56df88d9c
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-28 17:30:42 +0530

    fix: relative imports

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
-import { parse } from "../src/parser/index.ts";
-import { compile as compiler } from "../src/compiler/index.ts";
+import { parse } from "./src/parser/index.ts";
+import { compile as compiler } from "./src/compiler/index.ts";
 
 export function compile(code: string) {
   return compile(parse(code));
